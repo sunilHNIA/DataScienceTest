56c7094245f499cde302b7377b30c6faf5ae2cc8
sunilHNIA <162569269+sunilHNIA@users.noreply.github.com>
2024-05-21 15:31:21 +0100

    classtest.R

--- Untitled.R
+++ Untitled.R
@@ -1,4 +1,5 @@
 
+#Installing the required libraries
 if (!require(readr)) {
   install.packages("readr")
   library(readr)
@@ -9,32 +10,33 @@ if (!require(dplyr)) {
 }
 
 
-#Q1
+#Q1Creating the Retest_data dataframe
 Retest_data <- read_csv("/Users/sunilsharma/Desktop/untitled folder/big data architecture/Retest.csv")
+# to view the data frame in table format
 View(Retest_data)
 str(Retest_data)
 head(Retest_data, 15)
 num_rows <- nrow(Retest_data)
 cat("Number of rows in Retest_data:", num_rows)
 
-#Q2
+#Q2changing Datandtime variable from chr to date format
 Retest_data$dateandtime <- as.Date(Retest_data$dateandtime, format = "%m/%d/%Y")
 str(Retest_data)
 names(Retest_data)
 
-#q3
+#q3changing the specific columns names
 old_names <- c("dateandtime", "duration (hours/min).", "duration (seconds)", "value1", "value2")
 new_names <- c("DateTime", "TotalDuration", "DurationSeconds", "MeanValue_1", "MeanValue_2")
 names(Retest_data) <- ifelse(names(Retest_data) %in% old_names, new_names[match(names(Retest_data), old_names)], names(Retest_data))
 names(Retest_data)
 View(Retest_data)
 
-#Q4
+#Q4 Changing the MeanValue_2 to numeric from chr
 Retest_data <- mutate(Retest_data, MeanValue_2 = as.numeric(MeanValue_2))
 str(Retest_data)
 View(Retest_data)
 
-#q5
+#q5 installing mice and Vim libraries
 install.packages("mice")
 install.packages("VIM")
 library(mice)
@@ -71,7 +73,8 @@ records_deleted <- nrow(Retest_data) - nrow(cleaned_data)
 # Print the number of records deleted
 cat("Number of records deleted from the Retest data frame:", records_deleted)
 
-#q7
+#q7  plotting the chart 
+# summarizing the data using table function
 summary_table <- table(Retest_data$ID)
 
 # Plot the summary using a bar plot
@@ -80,19 +83,20 @@ plot(summary_table, type = "o",
      xlab = "ID",
      ylab = "MeanValue_1")
 
-#q8
+#q8 
+#sorting the data frame using arrange on shape then by city
 sorted_Retest_data <- Retest_data %>%
   arrange(shape, city)
 
-# Extract only the specified columns
+# Extracting only the specified columns
 sorted_Retest_data <- sorted_Retest_data %>%
   select(DateTime, city, country, shape)
 
-# Display the first 15 rows of data in the new data frame
+# Displaying the first 15 rows of data in the new data frame
 head(sorted_Retest_data, 15)
 str(sorted_Retest_data)                                                            
 
-#q9
+#q9Using the subset filter to filter where the country variable values are 'gb' and shape is 'disk'
 Retest_sub <- subset(Retest_data, country == "gb" & shape == "disk")
 
 # Count the total number of records in Retest_sub data frame
@@ -100,16 +104,7 @@ num_records <- nrow(Retest_sub)
 cat("Total number of records in Retest_sub data frame:", num_records)
 
 
-#q10
-write.csv(Retest_data, "modified_Retest.csv", row.names = FALSE)
-
-#Retest_sub data frame to CSV file
-write.csv(Retest_sub, "Retest_sub.csv", row.names = FALSE)
-
-#sorted_Retest_data data frame to CSV file
-write.csv(sorted_Retest_data, "sorted_Retest.csv", row.names = FALSE)
-
-
+#q10 
 #writing modified Retest data frame to CSV file
 write.csv(Retest_data, "/Users/sunilsharma/Desktop/DataScienceTest/modified_Retest.csv", row.names = FALSE)
 
@@ -120,7 +115,14 @@ write.csv(Retest_sub, "/Users/sunilsharma/Desktop/DataScienceTest/Retest_sub.csv
 write.csv(sorted_Retest_data, "/Users/sunilsharma/Desktop/DataScienceTest/sorted_Retest.csv", row.names = FALSE)
 
 
+# Adding all modified files to Github
+system("git add /Users/sunilsharma/Desktop/DataScienceTest/modified_Retest.csv")
+system("git add /Users/sunilsharma/Desktop/DataScienceTest/Retest_sub.csv")
+system("git add /Users/sunilsharma/Desktop/DataScienceTest/sorted_Retest.csv")
+system("git add YourScriptFileName.R")
 
+# Commit changes
+system("git commit -m 'Data Science class test'")
 
-
-                                                            
+# Push changes to remote GitHub repository
+system("git push origin master")
